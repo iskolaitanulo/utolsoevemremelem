0d98dcca8efaa5ed238468dfd677db464c18eb49
Sólya <dzsozef10@gmail.com>
2024-09-23 10:07:25 +0200

    megszakadok

---
+++ halozatprog/matekfeladatok.py
@@ -0,0 +1,20 @@
+szamlalo = int(input('Adja meg a szamalalojat'))
+nevezo = int(input('Adja meg a tört nevezőjét'))
+if szamlalo >= nevezo:
+    print(szamlalo/nevezo)
+
+elif szamlalo < nevezo:
+    print('Nem egész')
+negyzet = 1
+negyzetes = negyzet * negyzet
+
+while negyzetes <= 100000 :
+    print (negyzetes , end=', ')
+    negyzet = negyzet + 1 
+    negyzetes = negyzet ** 2
+
+
+prime = int(input('ADJON MEG EGY SZÁMOT VAGY RÁDESIK EGY HŰTŐ!!!!!!!!!'))
+
+if prime % 
+    
